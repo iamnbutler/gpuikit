e48c49e9d3339f3abe6ba6d63186ab8f67299ee2
iamnbutler <iamnbutler@github.com>
2025-11-22 14:17:45 -0500

    Fix theme init call

--- crates/gpuikit/src/lib.rs
+++ crates/gpuikit/src/lib.rs
@@ -16,6 +16,6 @@ pub mod utils;
 /// This must be called as soon as possible after your `gpui::Application` is created,
 /// as calling a gpuikit component before initialization will panic.
 pub fn init(cx: &mut App) {
-    gpuikit_theme::init(cx);
+    theme::init(cx);
     utils::element_manager::init(cx);
 }
